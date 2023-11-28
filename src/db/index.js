6e03ae97f7c42ca1f16d410fdd735f2ae98fcb6b
Ankita Kumari <ankitak0399@gmail.com>
2023-11-28 15:39:23 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const port = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+    try{
+       const connectionInstance =  await mongoose.connect(`${port}/${DB_NAME}`)
+       console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+    }
+    catch(error){
+        console.log("MONGODB connection failed", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,58 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv";
+dotenv.config({
+    path: ".env",
+});
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+connectDB();
+
+/*
 import express from "express";
 const app = express();
-const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World Ankita");
-})
 
-app.get('/about', (req, res) => {
-    res.send("About Page")
-})
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERRR: error");
+      throw error;
+    });
 
-app.get("/json", (req,res) => {
-    res.json({
-        name: "JOHN DOE",
-        age: 22,
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`)
     })
-})
+  } catch {
+    console.error("Error: ", error.message);
+    throw err;
+  }
+})();
+*/
 
-app.listen(port, () => {
-    console.log(`Server is listening to port ${port}`)
-});
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import express from "express";
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World Ankita");
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send("About Page")
+// })
+
+// app.get("/json", (req,res) => {
+//     res.json({
+//         name: "JOHN DOE",
+//         age: 22,
+//     })
+// })
+
+// app.listen(port, () => {
+//     console.log(`Server is listening to port ${port}`)
+// });
